87a9db250ee8db5d11073730cfc73ec933325545
AD0510 <112921114+AD0510@users.noreply.github.com>
2024-04-12 16:40:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+function getWeather() {
+    const apiKey = '40eecbee52483fa3c9beb148bc757d30'; 
+    const locationInput = document.getElementById('locationInput').value;
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${locationInput}&appid=${apiKey}&units=metric`;
+
+    fetch(apiUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            displayWeather(data);
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+        });
+}
+
+function displayWeather(data) {
+    const weatherInfo = document.getElementById('weatherInfo');
+    weatherInfo.innerHTML = `
+        <h2>${data.name}, ${data.sys.country}</h2>
+        <p>Temperature: ${data.main.temp}°C</p>
+        <p>Weather: ${data.weather[0].description}</p>
+        <p>Humidity: ${data.main.humidity}%</p>
+        <p>Wind Speed: ${data.wind.speed} m/s</p>
+    `;
+}
